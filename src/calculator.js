9892ae31e229b57cd5cefcbb492ffaca0a98c489
glinkinzhenya <glinkinzhenya@gmail.com>
2023-03-01 12:21:58 +0200

    1

---
+++ src/calculator.js
@@ -0,0 +1,8 @@
+const add = require(`./add`)
+const sub = require(`./sub`)
+const div = require(`./div`)
+const mult = require(`./mult`)
+
+const calculator = { add, sub, div, mult }
+
+module.exports = calculator;
